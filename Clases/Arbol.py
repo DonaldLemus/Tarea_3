d62f18ba684bc00b562ec9b4793dff2ff49fb876
DonaldLemus <79227467+DonaldLemus@users.noreply.github.com>
2023-02-24 22:36:01 -0600

    Add files via upload

---
+++ Clases/Arbol.py
@@ -0,0 +1,39 @@
+from Nodo import Nodo
+
+class Arbol:
+    #Constructor
+    def __init__(self):
+     self.raiz = None
+
+    #def = void cuando no tiene un return al final
+    def existe(self, busqueda):
+        return self.existe(self.raiz, busqueda)
+
+    
+    def existe(self, n, busqueda):
+        if n == None:
+            return False
+        if n.valorNodo() == busqueda:
+            return True
+        elif busqueda < n.valorNodo():
+            return self.existe(n.GetSubarbolIzdo(), busqueda)
+        else:
+            return self.existe(n.GetSubarbolDcho(), busqueda)
+
+    def insertar(self, dato):
+        if self.raiz == None:
+            self.raiz = Nodo(dato)
+        else:
+            self.insertar(self.raiz, dato)
+
+    def insertar(self, padre, dato):
+        if dato > padre.valorNodo():
+            if padre.GetSubarbolDcho() == None:
+                padre.SetRamaDcho(Nodo(dato))
+            else:
+                self.insertar(padre.GetSubarbolDcho(), dato)
+        else:
+            if padre.GetSubarbolIzdo() == None:
+                padre.SetRamaIzdo(Nodo(dato))
+            else:
+                self.insertar(padre.GetSubarbolIzdo(), dato)
